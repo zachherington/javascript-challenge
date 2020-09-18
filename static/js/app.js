3f62083bed5d64ccc02d0dba431281ef6448aa83
Zachary Herington <zachherington@gmail.com>
2020-09-18 00:14:12 -0400

    all data loading into web page. on to the filter functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,52 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//// Using D3 to select objects in the HTML code ////
+
+// Primary filter fields
+var output_table = d3.select("tbody"); 
+var filter_button = d3.select("#filter-btn");
+var date_criteria = d3.select("#datetime");
+
+//// Retrieve all Keys ////
+var key_list = Object.keys(tableData[0])
+    // console.log(key_list);
+
+
+//// Filling HTML Table with All Data ////
+var input_data = (data_source) => {
+    data_source.forEach(sighting => {
+        var new_row = output_table.append("tr");
+        
+        key_list.forEach(key => {
+            new_row.append("td").text(sighting[key]);
+    });
+});
+}
+
+input_data(tableData);
+
+
+
+//// Button Select Function ////
+// filter_button.on("click", function() {
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+//// Secondary filter fields (would need to all these form fields in the HTML)
+// var date_criteria = d3.select("#datetime");
+// var city_criteria = d3.select("#datetime");
+// var state_criteria = d3.select("#datetime");
+// var country_criteria = d3.select("#datetime");
+// var shape_criteria = d3.select("#datetime");
+// var comment_criteria = d3.select("#datetime");
